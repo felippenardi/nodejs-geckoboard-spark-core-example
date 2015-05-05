75f48baeb54a6ee846ca48e6f7f56faecad562c4
Felippe Nardi <felippe@nardi.me>
2015-05-05 14:45:47 -0400

    Add End-to-end test for an API interface

---
+++ test/api_users.js
@@ -0,0 +1,17 @@
+var hippie = require('hippie');
+var server = require('./../lib/Server');
+
+describe('Server', function () {
+  describe('/users endpoint', function () {
+    it('returns a user based on the id', function (done) {
+      hippie(server)
+        .json()
+        .get('/users/1')
+        .expectStatus(200)
+        .end(function(err, res, body) {
+          if (err) throw err;
+          done();
+        });
+    });
+  });
+});
